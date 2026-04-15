09d67d729bb3da87cd70923b385946395b0eebaa
gabriellycama <gabrielly.camargo.pereira@escola.pr.gov.br>
2026-04-15 15:13:55 -0300

    Add files via upload

---
+++ trabalhocomomatheus/gabi.py
@@ -0,0 +1,34 @@
+#atividade 1
+
+tri1 = 55
+tri2 = 61
+tri3 = 86
+resultado = tri1 + tri2 + tri3
+i = "matheus"
+
+if  resultado >= 180:
+    print("A pontuacao do aluno "+i+" foi de "+str(resultado)+" entao foi aprovado")
+else:
+    print("A pontuacao do aluno "+i+" foi de "+str(resultado)+" entao foi reprovado")
+
+
+#atividade2
+
+letras=input("digite uma dessas letras(A,T,C,G)").upper()
+if letras== 'A':
+    print('T')
+
+elif letras == 'T':
+    print('A')
+
+elif letras == 'C':
+    print('G')
+
+elif letras == 'G':
+    print('C')
+
+else:
+    print("letra invalida!")
+
+#atividade3
+
